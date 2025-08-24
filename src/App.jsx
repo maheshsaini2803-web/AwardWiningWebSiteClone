e6d3f1926d58e540c19f9d936f75a44ce211d862
JatinSainiOO7 <jatinsaini.developer@gmail.com>
2025-08-24 22:02:58 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <main>
+            <h1 className="text-5xl text-black">Hello World!</h1>
+        </main>
+    )
+}
+export default App;
